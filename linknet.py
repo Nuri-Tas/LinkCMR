f237f803dcc7187b27462873b754e4cac609fcf5
Can Koz <canxkoz@gmail.com>
2024-05-17 13:33:42 +0100

    linknet model upload

---
+++ linknet.py
@@ -0,0 +1,92 @@
+from torchvision.models import resnet
+from torch import nn
+
+class DecoderBlock(nn.Module):
+    def _init_(self, in_channels, n_filters):
+        super()._init_()
+
+        # B, C, H, W -> B, C/4, H, W
+        self.conv1 = nn.Conv2d(in_channels, in_channels // 4, 1)
+        self.norm1 = nn.BatchNorm2d(in_channels // 4)
+        self.relu1 = nn.ReLU(inplace=True)
+
+        # B, C/4, H, W -> B, C/4, H, W
+        self.deconv2 = nn.ConvTranspose2d(in_channels // 4, in_channels // 4, 3,
+                                          stride=2, padding=1, output_padding=1)
+        self.norm2 = nn.BatchNorm2d(in_channels // 4)
+        self.relu2 = nn.ReLU(inplace=True)
+
+        # B, C/4, H, W -> B, C, H, W
+        self.conv3 = nn.Conv2d(in_channels // 4, n_filters, 1)
+        self.norm3 = nn.BatchNorm2d(n_filters)
+        self.relu3 = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.norm1(x)
+        x = self.relu1(x)
+        x = self.deconv2(x)
+        x = self.norm2(x)
+        x = self.relu2(x)
+        x = self.conv3(x)
+        x = self.norm3(x)
+        x = self.relu3(x)
+        return x
+
+
+class LinkNet(nn.Module):
+    def _init_(self, num_classes, num_channels=3, encoder='resnet50'):
+        super()._init_()
+        assert encoder in ['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152']
+
+        filters = [256, 512, 1024, 2048]
+        
+        res = resnet.resnet101(pretrained=True)
+        
+        self.firstconv = res.conv1
+        self.firstbn = res.bn1
+        self.firstrelu = res.relu
+        self.firstmaxpool = res.maxpool
+        self.encoder1 = res.layer1
+        self.encoder2 = res.layer2
+        self.encoder3 = res.layer3
+        self.encoder4 = res.layer4
+
+        # Decoder
+        self.decoder4 = DecoderBlock(filters[3], filters[2])
+        self.decoder3 = DecoderBlock(filters[2], filters[1])
+        self.decoder2 = DecoderBlock(filters[1], filters[0])
+        self.decoder1 = DecoderBlock(filters[0], filters[0])
+
+        # Final Classifier
+        self.finaldeconv1 = nn.ConvTranspose2d(filters[0], 128, 3,
+                                               stride=2)
+        self.finalrelu1 = nn.ReLU(inplace=True)
+        self.finalconv2 = nn.Conv2d(128, 32, 3)
+        self.finalrelu2 = nn.ReLU(inplace=True)
+        self.finalconv3 = nn.Conv2d(32, num_classes, 2, padding=1)
+
+    def forward(self, x):
+        # Encoder
+        x = self.firstconv(x)
+        x = self.firstbn(x)
+        x = self.firstrelu(x)
+        x = self.firstmaxpool(x)
+        e1 = self.encoder1(x)
+        e2 = self.encoder2(e1)
+        e3 = self.encoder3(e2)
+        e4 = self.encoder4(e3)
+
+        # Decoder with Skip Connections
+        d4 = self.decoder4(e4) + e3
+        d3 = self.decoder3(d4) + e2
+        d2 = self.decoder2(d3) + e1
+        d1 = self.decoder1(d2)
+
+        # Final Classification
+        x = self.finaldeconv1(d1)
+        x = self.finalrelu1(x)
+        x = self.finalconv2(x)
+        x = self.finalrelu2(x)
+        x = self.finalconv3(x)
+        return x
